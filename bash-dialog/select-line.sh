cb63cdee4ff9aacee12d5de5389c64f3f9aece50
Paŭlo Ebermann <paul@fencing-game.de>
2011-03-22 23:02:16 +0100

    Test code to find out how to pass multiple lines as arguments.
    http://stackoverflow.com/questions/5393039/bash-dynamic-dialog/5397120#5397120

---
+++ bash-dialog/select-line.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+
+function showArgs () {
+    echo "---- $1 ----"
+    shift
+    while [[ $# > 0 ]]
+    do
+        echo "»$1«"
+        shift
+    done
+}
+
+function ignored () {
+echo "- example.txt ---"
+cat example.txt
+echo "- sed -----"
+sed -e 's/^.*$/"&" ""/' < example.txt
+
+
+showArgs '$(< ... )' $(< example.txt )
+
+showArgs "sed ''" $( sed -e "s/^.*$/'&' ''/" < example.txt )
+showArgs 'sed ""' $( sed -e 's/^.*$/"&" ""/' < example.txt )
+
+args=( $( sed -e 's/^.*@.*$/"&" ""/' < example.txt ) )
+
+showArgs "using array" "${args[@]}"
+
+(
+IFS=',' args=( $( sed -e 's/^.*$/&,_/' < example.txt ) )
+unset IFS
+
+showArgs "using array with special IFS" "${args[@]}"
+)
+
+arg="$(  sed -e "s/^.*$/'&' ''/" < example.txt )"
+showArgs "via variable" $arg
+showArgs "via variable" "$arg"
+
+# eval showArgs "'via eval'" "$arg"
+
+showArgs "direct parameter" "Name Surname mymail@mail.com" "" "Another New person@database.loc" ""
+}
+
+
+declare -a args=()
+
+cat example.txt | while read
+do
+    args+=("$REPLY" "")
+done
+
+showArgs "read + array" "${args[@]}"
+
+dialog --menu "Please select the line you want to edit" 40 60 34 "${args[@]}"
+
