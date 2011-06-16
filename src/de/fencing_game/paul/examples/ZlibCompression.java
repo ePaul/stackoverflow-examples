2213171521f6ac1159c799c1a3a41a05b542885a
Paŭlo Ebermann <paul@fencing-game.de>
2011-06-16 03:50:34 +0200

    ZlibCompression.
    Inspired by http://stackoverflow.com/q/6173920/600500 (and first
    published as an answer there).

---
+++ src/de/fencing_game/paul/examples/ZlibCompression.java
@@ -0,0 +1,63 @@
+package de.fencing_game.paul.examples;
+
+import java.util.zip.*;
+import java.io.*;
+
+/**
+ * Example program to demonstrate how to use zlib compression with
+ * Java.
+ * Inspired by http://stackoverflow.com/q/6173920/600500.
+ */
+public class ZlibCompression {
+
+    /**
+     * Compresses a file with zlib compression.
+     */
+    public static void compressFile(File raw, File compressed)
+        throws IOException
+    {
+        InputStream in = new FileInputStream(raw);
+        OutputStream out =
+            new DeflaterOutputStream(new FileOutputStream(compressed));
+        pipeInToOut(in, out);
+        in.close();
+        out.close();
+    }
+
+    /**
+     * Decompresses a zlib compressed file.
+     */
+    public static void decompressFile(File compressed, File raw)
+        throws IOException
+    {
+        InputStream in =
+            new InflaterInputStream(new FileInputStream(compressed));
+        OutputStream out = new FileOutputStream(raw);
+        pipeInToOut(in, out);
+        in.close();
+        out.close();
+    }
+
+    /**
+     * Shovels all data from an input stream to an output stream.
+     */
+    private static void shovelInToOut(InputStream in, OutputStream out)
+        throws IOException
+    {
+        byte[] buffer = new byte[1000];
+        int len;
+        while((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
+        }
+    }
+
+
+    /**
+     * Main method to test it all.
+     */
+    public static void main(String[] args) throws IOException, DataFormatException {
+        File compressed = new File("book1out.dfl");
+        compressFile(new File("book1"), compressed);
+        decompressFile(compressed, new File("decompressed.txt"));
+    }
+}
