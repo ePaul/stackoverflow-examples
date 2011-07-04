03b142e969949567ae9b4cbafcd60368b1eac2ae
Paŭlo Ebermann <paul@fencing-game.de>
2011-07-04 23:47:20 +0200

    download-test: simplify program, make serial
    Now it is testable, at least.

--- bash/parallelize/download-test.sh
+++ bash/parallelize/download-test.sh
@@ -4,23 +4,23 @@
 
 total=0
 getPages(){
-  subTotal=$(lynx -dump http://"$(printf "%s:%s" $1 $2)"/file.html | awk -F, 'NR==1 {print $1}' | sed 's/\s//g')
-  total=$(($total+$subTotal))
+    printf "%s:%s" $1 $2
+  subTotal=$(lynx -dump http://"$(printf "%s:%s" $1 $2)"/ | wc -l)
+  total=$(( $total + $subTotal ))
   echo "SubTotal: " $subTotal "Total: " $total
 }
 # /output/ SubTotal:  22 Total:  22
 # /output/ SubTotal:  48 Total:  48   //Note Total should be 70
 
 ARRAY=(
-'pf2.server.com:6599'
-'pf5.server.com:1199'
-...
+'www.gmx.de:80'
+'www.gmx.net:80'
 )
 
 for server in ${ARRAY[@]} ; do
   KEY=${server%%:*}
   VALUE=${server##*:}
-  getPages $KEY $VALUE &
+  getPages $KEY $VALUE
 done
 wait
   echo $total
