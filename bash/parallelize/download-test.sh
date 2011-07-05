60468980a1a3af4236822dd3189fc4679d39fbdd
Paŭlo Ebermann <paul@fencing-game.de>
2011-07-05 04:18:45 +0200

    download-test: now working concurrently

--- bash/parallelize/download-test.sh
+++ bash/parallelize/download-test.sh
@@ -2,28 +2,25 @@
 
 # from http://stackoverflow.com/q/6575492/600500
 
-total=0
-getPages(){
-    printf "%s:%s" $1 $2
-  subTotal=$(lynx -dump http://"$(printf "%s:%s" $1 $2)"/ | wc -l)
-  total=$(( $total + $subTotal ))
-  echo "SubTotal: " $subTotal "Total: " $total
-}
-# /output/ SubTotal:  22 Total:  22
-# /output/ SubTotal:  48 Total:  48   //Note Total should be 70
 
 ARRAY=(
 'www.gmx.de:80'
 'www.gmx.net:80'
+'www.gmx.at:80'
+'www.gmx.li:80'
 )
 
-for server in ${ARRAY[@]} ; do
-  KEY=${server%%:*}
-  VALUE=${server##*:}
-  getPages $KEY $VALUE
-done
-wait
-  echo $total
-exit 0        
 
-# /output/ 0
+(( total = 0 ))
+while read subtotal
+do
+   (( total = total + subtotal ))
+   echo "subtotal: $subtotal, total: $total"
+done < <(
+    pexec --number ${#ARRAY[*]} --normal-redirection --environment hostname --parameters ${ARRAY[@]} --shell-command -- '
+     lynx -dump http://$hostname/index.html | wc -l'
+    )
+
+echo "total: $total"
+
+exit 0
